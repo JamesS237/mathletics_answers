579fc27c3e8d0cb1d4f2c28f6ebabc6b6eedff15
James Spencer <jamess237@icloud.com>
2014-11-05 18:18:36 +1100

    all done

--- background.js
+++ background.js
@@ -1,6 +1,60 @@
-chrome.webRequest.onBeforeRequest.addListener(
-  function (details) {
-    console.log(JSON.stringify(details));
-  },
-  {urls: ["<all_urls>"]}
-);
+chrome.webRequest.onBeforeRequest.addListener(function(details) {
+  if(match(details.url)) {
+    getAnswer(details.url);
+  }
+}, {
+  urls: ["http://west.cdn.mathletics.com/*"]
+})
+
+chrome.webRequest.onBeforeSendHeaders.addListener(function (details){
+  if(match(details.url)) {
+    for (var i = 0; i < details.requestHeaders.length; ++i) {
+      if (details.requestHeaders[i].name === 'Access-Control-Allow-Origin') {
+        details.requestHeaders.splice(i, 1);
+        break;
+      }
+    }
+
+    return {requestHeaders: details.requestHeaders};
+  }
+}, {
+  urls: ["http://west.cdn.mathletics.com/*"]
+}, [
+  "blocking",
+  "requestHeaders"
+])
+
+function match(url) {
+  return !!url.match(/west.cdn.mathletics.com\/curriculum/) && !!url.match(/.QuestionXML.xml/) && !url.match(/me:9292/)
+}
+
+function getAnswer(url) {
+  url = "http://localhost:9292/"+ url.substring(7)
+
+  $.ajax({
+    type: "GET",
+    url: url,
+    dataType: "xml",
+    success: xmlParser
+   });
+
+  return true;
+}
+
+var notification;
+
+function xmlParser(xml) {
+  var answer = $(xml).find("actAnswerHashEnglish").html();
+
+  try {
+    answer = answer.split(';').join(', ')
+  } catch(e) {}
+
+  try {
+    answer = answer.split(',').join(', ')
+  } catch(e) {}
+
+  if(notification) notification.close();
+
+  notification = notify.createNotification("Mathletics Answer", {body: answer, icon: "icon.png"})
+}
